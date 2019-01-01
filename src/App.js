4cfe0e13358b3d79172aa63a747c1e0fd1f94946
itversity <itversity@Apples-MacBook-Pro-3.local>
2019-01-01 21:48:38 +0530

    Enabled CSS Loaders

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
-        <div className="App">
+        <div className={classes.App}>
           <h1>Hi, I'm a React App</h1>
           <p>This is really working!</p>
           <Person name="Max" age="28" />
