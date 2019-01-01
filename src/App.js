2e877e97f1a93396e5cb440bd3a0a61bd1a41746
itversity <itversity@Apples-MacBook-Pro-3.local>
2019-01-01 21:13:53 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div className="App">
+          <h1>Hi, I'm a React App</h1>
+          <p>This is really working!</p>
+          <Person name="Max" age="28" />
+          <Person name="Manu" age="29" >My Hobbies: Racing</Person>
+          <Person name="Stephanie" age="26" />
+        </div>
     );
   }
 }
---
+++ src/components/Summary.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Summary.css';
+import classes from './Summary.css';
+
+
+const summary = ( props ) => {
+    return (
+        <div className={classes.Summary}>
+            Summary of the profile
+        </div>
+    )
+};
+
+export default summary;
